d4e3b079abdceebdbbd41c8fc840cb0a095a6d39
Leo D. Penrose <lpenrose@depaul.edu>
2023-03-02 15:55:14 -0600

    Cleaned up HTML files and updated JS Logic

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const chosenNum = Math.floor(Math.random() * 100)
+const gameStatus = document.getElementById("game-status")
+console.log(chosenNum)
 
 function buttonClick() {
-  
+
   if (parseInt(num.value) === chosenNum) {
     console.log("You won!")
+    gameStatus.textContent = "You Won"
   }
   else if (parseInt(num.value) < chosenNum) {
     console.log("Too Low")
+    gameStatus.textContent = `${num.value} is Too LOW, Guess Again!`
   }
   else {
     console.log("Too High")
+    gameStatus.textContent = `${num.value} is Too HIGH, Guess Again!`
+
   }
 }
 
